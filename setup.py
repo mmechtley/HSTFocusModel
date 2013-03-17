e2f58766186b0b43b7e70060a35f125be379ef3b
Matt Mechtley <mmechtley@gmail.com>
2013-03-16 19:34:40 -0700

    Fixed error in distutils "requires" string

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ from distutils.core import setup
 setup(name='HSTFocusModel',
       version='0.2',
       py_modules=['HSTFocusModel'],
-      requires=['numpy >=1.4', 'scipy >=0.8']
-)
+      requires=['numpy (>=1.4)', 'scipy (>=0.8)'])
